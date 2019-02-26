4430b355cc2ccf96a530ce52d0aba9afe3d71ff5
alcamerone <al.camerone@gmail.com>
2019-02-27 12:39:50 +1300

    Add function to expose CPU usage

--- memory.go
+++ memory.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 /*
@@ -72,5 +74,57 @@ func GetMemoryUsageOfPid(id int) (usage MemoryUsage, err error) {
 	if err = scanner.Err(); err != nil {
 		return
 	}
+	return
+}
+
+func getCpuSample() (idle, total uint64, err error) {
+	f, err := os.Open("/proc/stat")
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					fmt.Println("Error: ", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			break
+		}
+	}
+
+	err = scanner.Err()
+	return
+}
+
+func GetCpuUsage() (usage float64, err error) {
+	idle0, total0, err0 := getCpuSample()
+	<-time.After(3 * time.Second)
+	idle1, total1, err1 := getCpuSample()
+
+	// Handle errors after sampling as sampling is time-sensitive
+	if err0 != nil {
+		err = err0
+		return
+	}
+	if err1 != nil {
+		err = err1
+		return
+	}
+
+	idleTicks := float64(idle1 - idle0)
+	totalTicks := float64(total1 - total0)
+	usage = (totalTicks - idleTicks) / totalTicks
 	return
 }
